32a95206b1c8761fdeb2643a2b163d334a7542c8
Shikhar Srivastava <shikharsrivastav@gmail.com>
2025-08-13 09:15:47 -0400

    update

--- static/js/index.js
+++ static/js/index.js
@@ -5,15 +5,15 @@ $(document).ready(function() {
     // Check for click events on the navbar burger icon
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
+		slidesToScroll: 1,
+		slidesToShow: 1,
+		loop: true,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 5000,
     }
 
-		// Initialize all div with carousel class
+	// Initialize all carousels
     var carousels = bulmaCarousel.attach('.carousel', options);
 	
     bulmaSlider.attach();
